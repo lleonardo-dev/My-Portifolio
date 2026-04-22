7279e22730abedf705198bfceabb6b0c51c4be62
Leonardo Augusto <Leonardoaugustofr1@gmail.com>
2026-04-22 20:43:30 -0300

    update of form

--- form.js
+++ form.js
@@ -1,1 +1,25 @@
-print
+ function falarcomigo() {
+            document.getElementById('contato').scrollIntoView({ behavior: 'smooth' });
+        }
+
+
+
+
+
+function  certificacoesjs() {
+
+   
+document.getElementById()
+
+
+
+}
+
+
+const nameinput = document.querySelector('user.name');
+const emailinput = document.querySelector('user.email');
+const messageinput = document.querySelector('user.message');
+const form = document.querySelector('form');
+
+
+
