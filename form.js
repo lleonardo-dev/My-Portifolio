ef17c553c539900637271e67132c0edfa1efdfb8
Leonardo <leonardoaugustofr1@gmail.com>
2026-04-24 09:33:06 -0300

    Add Node Js and biblioteca, Update estrutura and name html.

--- form.js
+++ form.js
@@ -1,25 +1,20 @@
- function falarcomigo() {
-            document.getElementById('contato').scrollIntoView({ behavior: 'smooth' });
-        }
+import { animate, scroll } from "motion";
 
+animate(
+  "#sobre",
+  { scale: [0.5, 1] }, // Isso anima DE 0.5 PARA 1
+  { ease: "circInOut", duration: 1 },
+);
 
-
-
-
-function  certificacoesjs() {
-
-   
-document.getElementById()
-
-
-
+function falarcomigo() {
+  document.getElementById("contato").scrollIntoView({ behavior: "smooth" });
 }
 
+function certificacoesjs() {
+  document.getElementById();
+}
 
-const nameinput = document.querySelector('user.name');
-const emailinput = document.querySelector('user.email');
-const messageinput = document.querySelector('user.message');
-const form = document.querySelector('form');
-
-
-
+const nameinput = document.querySelector("user.name");
+const emailinput = document.querySelector("user.email");
+const messageinput = document.querySelector("user.message");
+const form = document.querySelector("form");
